1b02c331cfa028dbbe193cc2b061263632340a9a
Thomas Pettersson <thomas.pettersson@actualize.de>
2025-08-14 10:01:34 +0200

    Add missing plugin core files
    - composer.json: Plugin definition and dependencies
    - Act1001FruchtTheme25.php: Main theme class
    - Required for Shopware plugin installation

---
+++ custom/plugins/Act1001FruchtTheme25/src/Act1001FruchtTheme25.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Act1001FruchtTheme25;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class Act1001FruchtTheme25 extends Plugin implements ThemeInterface
+{
+}
